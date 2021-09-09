f7044c64b26eea810d4a08c84d12d6d847bdf654
Dodo7999 <brat_miron@mail.ru>
2021-09-09 22:17:29 +0700

    FOCUSSTART-4 Получение и обновление данных о валютах

--- app/src/main/java/ru/focusstart/testtask/GetValuteService.kt
+++ app/src/main/java/ru/focusstart/testtask/GetValuteService.kt
@@ -1,11 +1,14 @@
 package ru.focusstart.testtask
 
+import kotlinx.coroutines.Deferred
 import retrofit2.Call
+import retrofit2.Response
 import retrofit2.http.GET
 import ru.focusstart.testtask.dto.CurrencyList
+import ru.focusstart.testtask.dto.ValuteDto
 
 interface GetValuteService {
 
     @GET("/daily_json.js")
-    fun getValutes(): Call<CurrencyList>
+    fun getValutes(): Deferred<Response<CurrencyList>>
 }
--- app/src/main/java/ru/focusstart/testtask/MainActivity.kt
+++ app/src/main/java/ru/focusstart/testtask/MainActivity.kt
@@ -2,41 +2,28 @@ package ru.focusstart.testtask
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.app.ProgressDialog
 import android.util.Log
-import android.widget.Toast
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
-import retrofit2.create
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import ru.focusstart.testtask.databinding.ActivityMainBinding
-import ru.focusstart.testtask.dto.CurrencyList
 
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private lateinit var currencyViewModel: CurrencyViewModel
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
 
-        val service = RetrofitClientInstance.retrofitInstance?.create(GetValuteService::class.java)
-        val call = service?.getValutes()
-        call?.enqueue(object : Callback<CurrencyList>{
-            override fun onResponse(call: Call<CurrencyList>, response: Response<CurrencyList>) {
-                val body = response.body()
-                val vatutes = body?.valute
-                Log.v("size", vatutes.toString())
-            }
-
-            override fun onFailure(call: Call<CurrencyList>, t: Throwable) {
-                Toast.makeText(applicationContext, "Error reading JSON", Toast.LENGTH_LONG).show()
-            }
-
+        currencyViewModel = ViewModelProvider(this).get(CurrencyViewModel::class.java)
+        currencyViewModel.fetchValute()
+        currencyViewModel.currency.observe(this, Observer {
+            binding.textView.text = it.toString()
+            Log.v("my", it.toString())
         })
-
     }
 
 
