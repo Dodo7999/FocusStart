38f817e72c68692690b66fd49e500b94569745d0
Dodo7999 <brat_miron@mail.ru>
2021-09-14 20:58:07 +0700

    good code

---
+++ app/src/main/java/ru/focusstart/testtask/presentation/CurrencyViewModel.kt
@@ -0,0 +1,47 @@
+package ru.focusstart.testtask.presentation
+
+import android.util.Log
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.*
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
+import ru.focusstart.networking.domain.entities.CurrencyEntities
+import ru.focusstart.networking.domain.repositories.CurrencyRepository
+import ru.focusstart.networking.domain.usecases.getRepositoryUseCase
+import ru.focusstart.testtask.presentation.state.MainState
+import kotlin.coroutines.CoroutineContext
+
+class CurrencyViewModel : ViewModel() {
+
+    private val parentJob = Job()
+
+    private val coroutineContext: CoroutineContext
+        get() = parentJob + Dispatchers.Default
+
+    private val scope = CoroutineScope(coroutineContext)
+
+    private val repository: CurrencyRepository = getRepositoryUseCase()
+
+    private val _uiState = MutableStateFlow<MainState>(MainState.Neutral)
+    val state: Flow<MainState>
+        get() = _uiState
+
+    val currency = MutableStateFlow<List<CurrencyEntities>>(emptyList())
+
+    private val handler = CoroutineExceptionHandler { _, _ ->
+        _uiState.value = MainState.Error
+    }
+
+    fun fetchValute() {
+        scope.launch(handler) {
+            _uiState.value = MainState.Loading
+            val valutes = repository.getCurrency()
+            currency.value = valutes ?: throw IllegalArgumentException("Currency is not be null")
+            _uiState.value = MainState.Success
+        }
+    }
+
+    fun cancelAllRequests() = coroutineContext.cancel()
+
+}
